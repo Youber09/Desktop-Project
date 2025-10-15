4b680ad9582ef7856a79ca8729ef6bd82a400a34
Youber09 <youber09@gmail.com>
2025-10-15 17:26:12 +0100

    Adding debounce time on creating tasks

--- App/src/routes/index.tsx
+++ App/src/routes/index.tsx
@@ -27,6 +27,7 @@ function RouteComponent() {
 
 
   const [task,setTask] = useState(``)
+  const [errorMessage,setErrorMessage] = useState(``)
   
 
   return <div className='bg-black w-full p-[5%] min-h-svh h-fit flex justify-center items-between'>
@@ -35,14 +36,27 @@ function RouteComponent() {
     <form onSubmit={ (e) => {e.preventDefault() ;}} >
 
       <div className='flex h-[5vw] w-[50vw] justify-center'>
-        <input type="text"  placeholder='task' className='bg-white/10 outline-0 p-[5%] rounded-l-[1vw] w-[30vw] text-[1.5vw] font-[Outfit] text-white' onChange={e => setTask(e.target.value)} />
+        <input type="text"  placeholder='task' className='bg-white/10 outline-0 p-[5%] rounded-l-[1vw] w-[30vw] text-[1.5vw] font-[Outfit] text-white' value={task} onChange={e => setTask(e.target.value)} />
         <button onClick={ async () => {
-            await createTask({Todo: task});
-            setRefresh(refresh + 1)
+            await setTimeout(async () => {
+              
+            
+
+              await createTask({Todo: task});
+              setErrorMessage(``)
+              setRefresh(refresh + 1);
+              if(task.length < 3){
+                setErrorMessage(`the task should be longer than 3 letters`)
+              } else{
+                setTask("")
+              }
+            }, 200);
           } } type='submit' className='text-white font-[Outfit] font-bold text-[1.5vw] bg-blue-600 px-[5%] rounded-r-[1vw] cursor-pointer hover:bg-blue-700 transition-all duration-200 group'>Add</button>
       </div>
 
-      <ul className=' mt-[5%] flex flex-col justify-center items-center space-y-[2%] w-[50vw]'>
+      <div className='text-[1.8vw] text-red-500 font-[Outfit] font-bold text-center mt-[2%]'>{errorMessage}</div>
+
+      <ul className=' mt-[3%] flex flex-col justify-center items-center space-y-[2%] w-[50vw]'>
         {
           tasks && tasks.Tasks.map((Todo : {task: string,id : string}) => <Task setRefresh={setRefresh} id={Todo.id} task={Todo.task} key={Todo.id} />)
         }
